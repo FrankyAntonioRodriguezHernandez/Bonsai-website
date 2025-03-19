e5b70141a100c16f758b67791266d61645e08873
Franky <frankyan2001@gmail.com>
2025-03-19 10:11:44 -0400

    Terminada la Hero-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroSectionComponent,
+    AboutComponent,
+    ServicesComponent,
+    GalleryComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    FooterComponent
+  ]
 })
 export class AppComponent {
-  title = 'bonsai-website';
+  title = 'La-Familia-del-Bonsai-website';
 }
